222a429451f757088d399ce7b5021ee669e60a5b
Xinyi YUAN <xyuan749@connect.hkust-gz.edu.cn>
2024-12-22 18:38:54 +0900

    LR update

--- 0000_test_programs/nn_ik/seed_jnt_selection_LR.py
+++ 0000_test_programs/nn_ik/seed_jnt_selection_LR.py
@@ -12,6 +12,7 @@ from torch.utils.data import DataLoader, TensorDataset
 import time
 from wrs import rm, mcm, wd
 
+from sklearn.svm import SVC
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
@@ -25,15 +26,14 @@ robot = cbt.Cobotta(pos=rm.vec(0.1,.3,.5), enable_cc=True)
 nupdate = 100
 trail_num = 100
 seed = 42
-mode = 'train' # ['train' or 'test','inference','ik_test']
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-backbone = 'LR' 
+backbone = 'SVM' 
 
 
 if __name__ == '__main__':
     '''save path'''
     TimeCode = ((datetime.now()).strftime("%m%d_%H%M")).replace(" ", "")
-    rootpath = f'{TimeCode}_{backbone}_seed_selection_large_limited'
+    rootpath = f'{TimeCode}_{backbone}_seed_selection_large'
     save_path = f'0000_test_programs/nn_ik/results/{rootpath}/'
     if os.path.exists(save_path) is False:
         os.makedirs(save_path)
@@ -52,17 +52,24 @@ if __name__ == '__main__':
     x_uniform_test = scaler.transform(x_test)
 
     '''model'''
-    # model = LogisticRegression()
-    model = LogisticRegression(class_weight='balanced', random_state=42)
+    if backbone == 'LR':
+        model = LogisticRegression(class_weight='balanced', random_state=42)
+    elif backbone == 'SVM':
+        # model = SVC(kernel='rbf', class_weight='balanced', probability=True, random_state=42)
+        model = SVC(kernel='rbf', probability=True, random_state=42)
 
     model.fit(x_uniform_train, y_train)
     y_pred = model.predict(x_uniform_test)
-    y_pred_proba = model.predict_proba(x_uniform_test)[:, 1]
+
+    if backbone == 'LR':
+        y_pred_proba = model.predict_proba(x_uniform_test)[:, 1]
+    elif backbone == 'SVM':
+        y_pred_proba = model.predict_proba(x_uniform_test)[:, 1]
 
     print("Accuracy:", accuracy_score(y_test, y_pred))
     print("\nClassification Report:\n", classification_report(y_test, y_pred))
 
-    model_path = os.path.join(save_path, 'logistic_regression_model.pkl')
+    model_path = os.path.join(save_path, f'{backbone.lower()}_model.pkl')
     scaler_path = os.path.join(save_path, 'scaler.pkl')
 
     joblib.dump(model, model_path)
