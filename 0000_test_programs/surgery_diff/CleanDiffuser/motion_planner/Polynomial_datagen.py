5fc225bb802989114babb2757ca0a62d1059b488
Xinyi YUAN <xyuan749@connect.hkust-gz.edu.cn>
2025-05-14 20:40:09 +0900

    save after t-->s mapping

--- 0000_test_programs/surgery_diff/CleanDiffuser/motion_planner/Polynomial_datagen.py
+++ 0000_test_programs/surgery_diff/CleanDiffuser/motion_planner/Polynomial_datagen.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import make_lsq_spline, BSpline
+from scipy.optimize import minimize
+import zarr
+from tqdm import tqdm
 
 import zarr
 import numpy as np
@@ -20,11 +23,49 @@ print('dataset created in:', dataset_name)
 meta_group = root.create_group("meta")
 data_group = root.create_group("data")
 episode_ends_ds = meta_group.create_dataset("episode_ends", shape=(0,), chunks=(1,), dtype=np.float32, append=True)
-poly_coef_ds = data_group.create_dataset("control_points", shape=(0, 9), chunks=(1, 9), dtype=np.float32, append=True)
+poly_coef_ds = data_group.create_dataset("poly_coef", shape=(0, 9), chunks=(1, 9), dtype=np.float32, append=True)
 
 '''polynomial parameter'''
 degree = 8
 dt = 0.01
+
+def constrained_polynomial_fit(s, y, degree):
+    """
+    基于 s 参数化 0-1 的多项式拟合，带约束：位置、速度、加速度和抖动在起始和末尾为 0。
+    """
+    p_init = np.polyfit(s, y, degree)
+    
+    def objective(p):
+        poly = np.poly1d(p)
+        residuals = np.sum((poly(s) - y) ** 2)
+        return residuals
+
+    # 约束：起始和终止的速度、加速度、抖动和位置
+    def position_constraint(p):
+        poly = np.poly1d(p)
+        return [poly(0) - y[0], poly(1) - y[-1]]
+
+    def velocity_constraint(p):
+        poly = np.poly1d(p)
+        return [np.polyder(poly, 1)(0), np.polyder(poly, 1)(1)]
+
+    def acceleration_constraint(p):
+        poly = np.poly1d(p)
+        return [np.polyder(poly, 2)(0), np.polyder(poly, 2)(1)]
+
+    def jerk_constraint(p):
+        poly = np.poly1d(p)
+        return [np.polyder(poly, 3)(0), np.polyder(poly, 3)(1)]
+
+    constraints = [
+        {"type": "eq", "fun": position_constraint},
+        {"type": "eq", "fun": velocity_constraint},
+        {"type": "eq", "fun": acceleration_constraint},
+        {"type": "eq", "fun": jerk_constraint}
+    ]
+
+    result = minimize(objective, p_init, constraints=constraints, method='SLSQP')
+    return np.poly1d(result.x)
 
 episode_ends_counter = 0
 for traj_id in range(len(ruckig_root['meta']['episode_ends'][:])):
@@ -42,23 +83,11 @@ for traj_id in range(len(ruckig_root['meta']['episode_ends'][:])):
     t = np.linspace(0, (T - 1) * dt, T)
     num_joints = jnt_pos_list.shape[1]  # recconstruct multiple joints
     s = np.linspace(0, 1, T)
-    
-    '''approximate b-spline'''
-    ctrl_values_list = []
 
     for j in range(num_joints):
-        x = jnt_pos_list[:, j]
-        ctrl_values = np.interp(ctrl_points, s, x)
-        ctrl_values[0] = x[0]  # make sure start aligned
-        ctrl_values[-1] = x[-1]  # make sure end aligned
-        ctrl_values_list.append(ctrl_values)
-
-    # control points to 2D array (each column is a joint)
-    ctrl_values_matrix = np.vstack(ctrl_values_list).T
-    spline = make_lsq_spline(s, jnt_pos_list, knots, degree)
-
-    '''print parameters and save into zarr'''
-    print(f'c shape: {spline.c.shape}')
-    control_points_ds.append(np.array(spline.c))
-    episode_ends_counter += len(spline.c)
+        episode_ends_counter += 1
+        y = jnt_pos_list[:, j]
+        poly_s = constrained_polynomial_fit(s, y, degree)
+        poly_coef_ds.append((np.poly1d(poly_s).coefficients).reshape(1, 9))
+    
     episode_ends_ds.append(np.array([episode_ends_counter], dtype=np.int32))
