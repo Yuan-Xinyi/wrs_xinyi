ac2a355663111465435c0f68f801e19030f9dc90
Xinyi YUAN <xyuan749@connect.hkust-gz.edu.cn>
2025-05-20 13:14:58 +0900

    add inference code

--- 0000_test_programs/surgery_diff/CleanDiffuser/motion_planner/ruckig_chiunt_polynomial.py
+++ 0000_test_programs/surgery_diff/CleanDiffuser/motion_planner/ruckig_chiunt_polynomial.py
@@ -46,6 +46,50 @@ def pad_to_power_of_two(x):
         x_padded = torch.nn.functional.pad(x, (0, 0, 0, padding_size), mode='constant', value=0)
         return x_padded
 
+def plot_polynomial_trajectories(poly_coef, robot_s_n_dof, T = 10, dt=0.01, plot_derivatives=False):
+    t = np.linspace(0, T, int(T / dt))
+    s = np.linspace(0, 1, len(t))
+    
+    fig, axs = plt.subplots(robot_s_n_dof, 4, figsize=(16, 4 * robot_s_n_dof), sharex=True)
+    
+    for dof in range(robot_s_n_dof):
+        poly = np.poly1d(poly_coef[dof])  # 生成多项式
+        y = poly(s)                       # 计算多项式轨迹
+        dy_ds = np.polyder(poly, 1)(s)
+        d2y_ds2 = np.polyder(poly, 2)(s)
+        d3y_ds3 = np.polyder(poly, 3)(s)
+
+        # 映射到时间
+        dy_dt = dy_ds / T
+        d2y_dt2 = d2y_ds2 / (T ** 2)
+        d3q_dt3 = d3y_ds3 / (T ** 3)
+        
+        # 绘制原始数据 vs 参数化拟合
+        axs[dof, 0].plot(t, y, label='Parametric Fit', linewidth=2, color='red')
+        axs[dof, 0].set_title(f"Position $q_{dof}(t)$")
+        axs[dof, 0].set_ylabel("Position")
+        axs[dof, 0].legend()
+
+        # 速度（Velocity）
+        axs[dof, 1].plot(t, dy_dt, label='Parametric Velocity', linewidth=2, color='red')
+        axs[dof, 1].set_title(f"Velocity $\\dot{{q}}_{dof}(t)$")
+        axs[dof, 1].set_ylabel("Velocity")
+        axs[dof, 1].legend()
+
+        # 加速度（Acceleration）
+        axs[dof, 2].plot(t, d2y_dt2, label='Parametric Acceleration', linewidth=2, color='red')
+        axs[dof, 2].set_title(f"Acceleration $\\ddot{{q}}_{dof}(t)$")
+        axs[dof, 2].set_ylabel("Acceleration")
+        axs[dof, 2].legend()
+
+        # 抖动（Jerk）
+        axs[dof, 3].plot(t, d3q_dt3, label='Parametric Jerk', linewidth=2, color='red')
+        axs[dof, 3].set_title(f"Jerk $\\dddot{{q}}_{dof}(t)$")
+        axs[dof, 3].set_ylabel("Jerk")
+        axs[dof, 3].legend()
+    plt.tight_layout()
+    plt.show()
+
 def set_seed(seed: int):
     torch.manual_seed(seed)
     np.random.seed(seed)
@@ -91,11 +135,6 @@ if config['nn'] == "chi_unet":
         obs_as_global_cond=True, timestep_emb_type="positional").to(config['device'])
 elif config['nn'] == "chi_transformer":
     from cleandiffuser.nn_diffusion import ChiTransformer
-    '''for small model'''
-    # nn_diffusion = ChiTransformer(
-    #         config['action_dim'],config['obs_dim'], config['horizon'], config['obs_steps'], d_model=256, nhead=4, num_layers=4,
-    #         timestep_emb_type="positional").to(config['device'])
-    '''for large model'''
     nn_diffusion = ChiTransformer(
             config['action_dim'],config['obs_dim'], config['horizon'], config['obs_steps'], d_model=320, nhead=10, num_layers=8,
             timestep_emb_type="positional").to(config['device'])
@@ -119,9 +158,12 @@ report_parameters(nn_diffusion)
 import wrs.robot_sim.robots.franka_research_3.franka_research_3 as franka
 from wrs import wd, rm, mcm
 robot_s = franka.FrankaResearch3(enable_cc=True)
+x_max = torch.ones((1, config['horizon'], config['action_dim']), device=config['device']) * +1.0
+x_min = torch.ones((1, config['horizon'], config['action_dim']), device=config['device']) * -1.0
 
 agent = DDPM(
     nn_diffusion=nn_diffusion, nn_condition=nn_condition,
+    x_max=x_max, x_min=x_min,
     device=config['device'], diffusion_steps=config['diffusion_steps'],
     optim_params={"lr": config['lr']}, predict_noise=config['predict_noise'])
 
@@ -131,7 +173,7 @@ if config['mode'] == "train":
     # --------------- Data Loading -----------------
     '''prepare the save path'''
     TimeCode = ((datetime.now()).strftime("%m%d_%H%M")).replace(" ", "")
-    rootpath = f"{TimeCode}_{config['horizon']}h_norm{config['normalize']}_poly_{config['nn']}"
+    rootpath = f"{TimeCode}_{config['horizon']}h_norm{config['normalize']}_poly_{config['nn']}s"
     save_path = os.path.join(current_file_dir, 'results', rootpath)
     if not os.path.exists(save_path):
         os.makedirs(save_path)
@@ -183,11 +225,11 @@ if config['mode'] == "train":
     wandb.finish()
 
 if config['mode'] == "inference":
-    raise NotImplementedError("Inference mode is not implemented yet")
+    # raise NotImplementedError("Inference mode is not implemented yet")
     from scipy.interpolate import make_lsq_spline, BSpline
 
     model_path = '0000_test_programs/surgery_diff/CleanDiffuser/motion_planner/results/' \
-    '0512_2022_64h_64b_normFalse/diffusion_ckpt_latest.pt'
+    '0519_1753_7h_normTrue_poly_chi_transformer/diffusion_ckpt_latest.pt'
     agent.load(model_path)
     agent.model.eval()
     agent.model_ema.eval()
@@ -204,17 +246,13 @@ if config['mode'] == "inference":
     base = wd.World(cam_pos=[2, 0, 1], lookat_pos=[0, 0, 0])
     mgm.gen_frame().attach_to(base)
 
-    # init the b-spline parameter
-    degree = 4
-    num_ctrl_pts = 64
-    ctrl_points = np.linspace(0, 1, num_ctrl_pts)
-    knots = np.linspace(0, 1, num_ctrl_pts - degree + 1)
-    knots = np.concatenate(([0] * degree, knots, [1] * degree))
+    # init the poly parameter
+    degree = 7
+    dt = 0.01
 
     # inference
     solver = config['inference_solver']
     inference_steps = config['inference_steps']
-    visualization = config['visualization']
     
     success_num = 0
     n_samples = 1
@@ -232,18 +270,20 @@ if config['mode'] == "inference":
     #     goal_list.append(control_points[-1])
 
     '''single traj test'''
-    root = zarr.open('/home/lqin/zarr_datasets/franka_ruckig_100hz_bspline.zarr', mode='r')
     traj_id = 0
     traj_end = int(root['meta']['episode_ends'][traj_id])
-    control_points = root['data']['control_points'][traj_end-64:traj_end]
+    poly_coef = root['data']['poly_coef'][traj_end-config['horizon']:traj_end]
+    start_conf = root['data']['start_conf'][traj_end-config['horizon']:traj_end]
+    goal_conf = root['data']['goal_conf'][traj_end-config['horizon']:traj_end]
+
     
     # --------------- Inference -----------------
     # for traj_id in tqdm(range(len(start_list))):
     for traj_id in range(1):
         '''prepare the start and goal config'''
         # start_conf, goal_conf = start_list[traj_id], goal_list[traj_id]
-        start_conf = control_points[0]
-        goal_conf = control_points[-1]
+        start_conf = start_conf[0]
+        goal_conf = goal_conf[0]
         # start_conf = robot_s.rand_conf()
         # goal_conf = robot_s.rand_conf()
         # start_conf = np.array([ 1.00625858,  0.15694599, -2.51097176, -1.60473395,  0.40952758,
@@ -260,36 +300,36 @@ if config['mode'] == "inference":
         robot_s.goto_given_conf(jnt_values=start_conf)
         robot_s.gen_meshmodel(alpha=0.2, rgb=[0,0,1]).attach_to(base)
 
+        '''validate the normalization'''
+        if config['normalize']:
+            start_conf  = dataset.normalizer['obs']['jnt_pos'].normalize(start_conf)
+            goal_conf = dataset.normalizer['obs']['jnt_pos'].normalize(goal_conf)
+
         '''inference the trajectory'''
-        condition = None
         prior = torch.zeros((1, config['horizon'], config['action_dim']), device=config['device'])
-        prior[:, 0, :] = torch.tensor(start_conf).to(config['device'])
-        prior[:, -1, :] = torch.tensor(goal_conf).to(config['device'])
+        if config['condition'] == "identity":
+            condition = np.stack([start_conf, goal_conf], axis=0)
+            if config['nn'] == 'chi_unet' or config['nn'] == 'dit':
+                condition = condition.flatten(start_dim=1)
+        else:
+            condition = None
+        condition = torch.from_numpy(condition).to(config['device'])
         with torch.no_grad():
             action, _ = agent.sample(prior=prior, n_samples=n_samples, sample_steps=config['sample_steps'], temperature=0.1,
                                     solver=solver, condition_cfg=condition, use_ema=True)
         
+        '''post-process the action'''
+        np.set_printoptions(precision=3, suppress=True, linewidth=120)
+        print(f'Original unnormalized action: \n{action[0]}')
+        action = action.cpu().numpy()
+        action = dataset.normalizer['action']['poly_coef'].unnormalize(action)
+        action = action[0]
+        poly_coef = np.zeros((7, degree+1))
+        poly_coef[:, :4] = action[:, :4]   # 保留前 4 列
+        poly_coef[:, 7:] = action[:, 4:] 
+        print(f"Coef from diffusion: \n{action}")
+        print(f"Full Reconstructed Coef: \n{poly_coef}")
+        plot_polynomial_trajectories(poly_coef, robot_s_n_dof=robot_s.n_dof, plot_derivatives=True)
 
-        # '''recons b-spline'''
-        # # spline = BSpline(knots, control_points, degree)
-        # spline = BSpline(knots, action[0].cpu().numpy().copy(), degree)
-        # print(repr(action[0].cpu().numpy().copy()))
-        T_total_list = [5]
-        # results = []
-
-        # for T_total_new in T_total_list:
-        #     print(f"\nTesting with T_total = {T_total_new}s")
-        #     result = (T_total_new, *update_bspline_with_new_time(spline, T_total_new))
-        #     results.append(result)
-
-        # plot_bspline(results, robot_s.n_dof, overlay=True)
-        # plot_control_points_comparison(control_points, action[0].cpu().numpy().copy(), robot_s.n_dof)
-        
-        '''compare the action with the control points'''
-        generated_c = action[0].cpu().numpy().copy()
-        generated_c[:4] = control_points[:4]
-        generated_c[-4:] = control_points[-4:]
-        compare_bspline_two_methods(knots, control_points, generated_c, 
-                                    degree, T_total_list, robot_s.n_dof)
 else:
     raise ValueError("Illegal mode")
